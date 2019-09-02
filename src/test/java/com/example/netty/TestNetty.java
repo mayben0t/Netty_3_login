0e5990b7bbdbe0f82e1472a639255d861df6ca70
王骁 <wangxiao@koolearn.com>
2019-09-02 11:26:36 +0800

    去除test

--- src/test/java/com/example/netty/TestNetty.java
+++ src/test/java/com/example/netty/TestNetty.java
@@ -2,24 +2,15 @@ package com.example.netty;
 
 
 import io.netty.buffer.ByteBuf;
-import nettyp.byteBuf.Encode;
-import nettyp.deSeializer.DecodeWork;
-import nettyp.serializer.JsonSerializer;
-import nettyp.serializer.RequestMsg;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestNetty {
 
+
     @Test
     public void test(){
-        RequestMsg req = new RequestMsg(1, "王骁", 24);
-
-        ByteBuf byteBuf = Encode.getByteBuf(req);
-        RequestMsg requestMsg = DecodeWork.decodeByteBuf(byteBuf);
-
-        Assert.assertEquals(req,requestMsg);
-        System.out.println(requestMsg);
 
     }
+
 }
